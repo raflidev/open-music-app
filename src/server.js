36d5e75bf1bc5fa5e0f5db68040009d046c97516
Rafli Ramadhan <rafliramdhn@gmail.com>
2022-05-05 14:15:34 +0700

    membuat struktur with hapi plugin

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
+const albums = require('./api/albums');
+const AlbumsService = require('./services/inMemory/AlbumsService');
 
 const init = async () => {
+  const albumsService = new AlbumsService();
+
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -13,6 +17,13 @@ const init = async () => {
   });
 
   server.route(routes);
+
+  await server.register({
+    plugin: albums,
+    options: {
+      service: albumsService,
+    }
+  })
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
